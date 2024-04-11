ab0a74c2b6ae4002f3c1aa7088fec57ee57d261a
Julianthomaspeniel <145668396+Julianthomaspeniel@users.noreply.github.com>
2024-04-11 17:19:37 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,11 +1,15 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
-
 class User(BaseModel):
     username: str
     email: str
     password: str
+
+class ShowUser(BaseModel):
+    username: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
 
 class Profile(BaseModel):
     firstname: str
@@ -13,6 +17,13 @@ class Profile(BaseModel):
     age: int
     dob: str
     phone_number: int
+
+class ShowProfile(BaseModel):
+    firstname: Optional[str] = None
+    lastname: Optional[str] = None
+    age: Optional[int] = None
+    dob: Optional[str] = None
+    phone_number: Optional[int] = None
 
 class Address(BaseModel):
     door_no: str
@@ -22,6 +33,15 @@ class Address(BaseModel):
     postal_code: str
     country: str
     address_type: str
+
+class ShowAddress(BaseModel):
+    door_no: Optional[str] = None
+    street: Optional[str] = None
+    city: Optional[str] = None
+    state: Optional[str] = None
+    postal_code: Optional[str] = None
+    country: Optional[str] = None
+    address_type: Optional[str] = None
  
 class Login(BaseModel):
     username: str
